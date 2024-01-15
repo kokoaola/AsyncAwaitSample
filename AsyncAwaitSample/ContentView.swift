747ebe2a748a9428c4b1fb4e0b9318a47b1b2ab6
koala panda <koakoa8823@protonmail.com>
2024-01-15 23:30:43 +0900

    プロジェクトの追加

---
+++ AsyncAwaitSample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AsyncAwaitSample
+//
+//  Created by koala panda on 2024/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
